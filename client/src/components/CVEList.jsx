3240d273754bb4ee99d72032575014695b5b15e8
Sid Sancheti <sidsancheti94@gmail.com>
2025-02-06 00:55:33 -0500

    Linter

--- client/src/components/CVEList.jsx
+++ client/src/components/CVEList.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import '../styles/CVEList.css';
+import React from "react";
+import "../styles/CVEList.css";
 
 function CVEList() {
   return (
     <div className="App">
-    <header className="App-header">
-      <h1 className="App-h1">CVE LIST</h1>
-    </header>
-    <p>Total Records: 1234</p>
-    <table>
-      <thead>
-        <tr>
-          <th>CVE ID</th>
-          <th>Identifier</th>
-          <th>Published Date</th>
-          <th>Last Modified Date</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Data 1</td>
-          <td>Data 2</td>
-          <td>Data 3</td>
-          <td>Data 3</td>
-          <td>Data 3</td>
-        </tr>
-        <tr>
-          <td>Data 4</td>
-          <td>Data 5</td>
-          <td>Data 6</td>
-          <td>Data 3</td>
-          <td>Data 3</td>
-        </tr>
-      </tbody>
-    </table>
+      <header className="App-header">
+        <h1 className="App-h1">CVE LIST</h1>
+      </header>
+      <p>Total Records: 1234</p>
+      <table>
+        <thead>
+          <tr>
+            <th>CVE ID</th>
+            <th>Identifier</th>
+            <th>Published Date</th>
+            <th>Last Modified Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <td>Data 3</td>
+            <td>Data 3</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 3</td>
+            <td>Data 3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
